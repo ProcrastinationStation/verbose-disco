9653dbcd4338f2360782c245b1da17e02f59f5d1
ProcrastinationStation <lukka.ivanovic@gmail.com>
2020-09-08 12:03:36 +0200

    master

---
+++ app/src/main/java/UI/LoginActivity.kt
@@ -0,0 +1,17 @@
+package UI
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.stockcourt.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClicked(view: View) {
+
+    }
+}
---
+++ app/src/main/java/UI/ui/TrendsActivity.kt
@@ -0,0 +1,12 @@
+package UI.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stockcourt.R
+
+class TrendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trends)
+    }
+}
